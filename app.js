dd204ade746cf6c9bcfffbe7686b9d0002fc3df3
LAPTOP-QJBRI58J\Anne <koala1995610@gmail.com>
2022-12-25 23:02:01 +0800

    feat: add rendering web page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const exphbs = require("express-handlebars")
 const port = 3000
 
 const app = express()
-app.engine("hbs", exphbs({ defaultLayouts: "main", extname: ".hbs" }))
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }))
 app.set("view engine", "hbs")
 
 app.get("/", (req, res) => {
